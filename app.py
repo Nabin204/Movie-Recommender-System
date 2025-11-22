dcbcc4405465c07090009b282198fdede28d6547
Nabin Bhat <nabinbhat204@gmail.com>
2025-11-22 09:03:13 +0545

    Second Commit

--- app.py
+++ app.py
@@ -13,37 +13,37 @@ API_KEY = os.getenv("TMDB_API_KEY")
 
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US"
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/original/" + poster_path
-    return full_path
+    responce = requests.get(url)
+    data = responce.json()
+    return "https://image.tmdb.org/t/p/original/" + data['poster_path']
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
     recommended_movies_posters = []
-    for i in movie_list:
+    for i in movies_list:
         # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
-        recommended_movies_posters.append(fetch_poster(movie_id))
+
         recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
+
 
     return recommended_movies,recommended_movies_posters
 
 
 movies_dict = pickle.load(open('movies_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
+
 similarity = pickle.load(open('similarity.pkl','rb'))
 st.title('Movie Recommender System')
 
-movie_list = movies['title'].values
 selected_movie_name = st.selectbox(
     "Type or select a movie from the dropdown",
-    movie_list
+    movies['title'].values
 )
 
 if st.button('Recommend'):
